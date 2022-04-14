7a6818830e46ba87e8f8550112ae64bbd8ab7746
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2022-04-14 15:19:58 +0100

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""
-Script that lists all State objects from the database
-"""
-
-from sys import argv
-from model_state import Base, State
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+"""adds the State object Louisiana
+to the database hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
 
-    user = argv[1]
-    password = argv[2]
-    database = argv[3]
-    state_name = argv[4]
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (user, password, database), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    state = session.query(State).filter(State.name == state_name).first()
-    if state is not None:
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    if state:
         print("{}".format(state.id))
     else:
         print("Not found")
