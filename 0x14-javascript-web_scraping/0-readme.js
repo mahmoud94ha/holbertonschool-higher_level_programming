7a8bb5c938313f4b9fd91a148ea5a2c358ff0c32
mahmoud94ha <mahmoudhannani3@icloud.com>
2022-05-17 20:47:14 +0000

    another

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
-const fileList = event.target.files;
-let fileContent = "";
-
-const fr = new FileReader();
-fr.onload = () => {
-  fileContent = fr.result;
-  console.log('Commands', fileContent);
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
 }
-
-fr.readAsText(fileList[0]);
