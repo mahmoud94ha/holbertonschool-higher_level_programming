92ca004ad29a6345fd2091775bbb10fc2a4d9347
mahmoud94ha <mahmoudhannani3@icloud.com>
2022-05-17 21:06:28 +0000

    takes string write t to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs = required('fs');
+const fs = require('fs');
 
 const data = process.argv[3];
 
-fs.wirteFile(process.argv[2], data, function (err, data) {
+fs.writeFile(process.argv[2], data, function (err, data) {
 	if (err) console.log(err);
 }Ã);
