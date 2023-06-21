5478b3f47e9512756fb24a726b568c4c0fe461b2
mahmoud94ha <mahmoud94hau@gmail.com>
2023-06-21 03:19:42 +0100

    Factor index

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const newList = list.map((element, index) => element * index);
+const newList = list.map((value, index) => value * index);
 console.log(list);
 console.log(newList);
