910fafafeddf2d61da77b74a8c2437dc239c0941
mahmoud94ha <mahmoudhannani3@gmail.com>
2023-06-24 01:48:17 +0100

    add

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+// This script computes the number of tasks completed by user id.
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+  } else {
+    const movie = JSON.parse(body);
+
+    const characterUrls = movie.characters;
+
+    characterUrls.forEach(characterUrl => {
+      request.get(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error('Error:', error);
+        } else if (response.statusCode !== 200) {
+          console.error('Status:', response.statusCode);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  }
+});
