974bac992228284f1819772b2cb3409df8a6bc22
mahmoud94ha <mahmoudhannani3@gmail.com>
2023-06-24 02:12:55 +0100

    add

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -7,25 +7,18 @@ const destinationFile = process.argv[4];
 
 fs.readFile(sourceFile1, 'utf8', (error, data1) => {
   if (error) {
-    console.error(`Error reading ${sourceFile1}:`, error);
     return;
   }
 
   fs.readFile(sourceFile2, 'utf8', (error, data2) => {
     if (error) {
-      console.error(`Error reading ${sourceFile2}:`, error);
       return;
     }
-    
     const concatenatedData = data1 + data2;
-
     fs.writeFile(destinationFile, concatenatedData, 'utf8', (error) => {
       if (error) {
-        console.error(`Error writing to ${destinationFile}:`, error);
         return;
       }
-
-      console.log(`Successfully concatenated ${sourceFile1} and ${sourceFile2} into ${destinationFile}`);
     });
   });
 });
